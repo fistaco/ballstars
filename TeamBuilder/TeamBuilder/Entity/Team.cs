5bc01c76abc0d91ccd9a7664f160b086ef76228c
fistaco <fiske_schijlen@hotmail.com>
2020-01-02 17:54:47 +0100

    Define a Team as a set of players. Doesn't include fitness-related methods yet.

---
+++ TeamBuilder/TeamBuilder/Entity/Team.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TeamBuilder.Entity
+{
+    internal class Team
+    {
+        public List<Player> Members;
+
+        public Team(List<Player> members)
+        {
+            this.Members = members;
+        }
+    }
+}
