0db9ba4fdfec435096fb156f992ca5c683389197
fistaco <fiske_schijlen@hotmail.com>
2020-01-02 18:47:50 +0100

    Keep constant track of a team's gender imbalance.

--- TeamBuilder/TeamBuilder/Entity/Player.cs
+++ TeamBuilder/TeamBuilder/Entity/Player.cs
@@ -8,13 +8,22 @@ namespace TeamBuilder.Entity
         public Sport Sport;
         public bool InOrganisation;
 
-
         public Player(string name, string gender, string sportName, bool inOrganisation)
         {
             this.Name = name;
             this.Gender = gender == "Male" ? Gender.Male : Gender.Female;
             this.Sport = Globals.SportsMap[sportName];
             this.InOrganisation = inOrganisation;
+        }
+
+        public bool IsMale()
+        {
+            return this.Gender == Gender.Male;
+        }
+
+        public bool IsFemale()
+        {
+            return this.Gender == Gender.Female;
         }
     }
 }
--- TeamBuilder/TeamBuilder/Entity/Team.cs
+++ TeamBuilder/TeamBuilder/Entity/Team.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TeamBuilder.Entity
 {
@@ -6,9 +7,40 @@ namespace TeamBuilder.Entity
     {
         public List<Player> Members;
 
+        public int GenderImbalance = 0;
+        private readonly Dictionary<Gender, int> _genderCounts = new Dictionary<Gender, int>()
+        {
+            { Gender.Male, 0 },
+            { Gender.Female, 0 }
+        };
+
+        public int AmountOfOrganisers = 0;
+
+        public int SportImbalance = 0;
+
         public Team(List<Player> members)
         {
-            this.Members = members;
+            foreach (Player p in members)
+            {
+                this.AddPlayer(p);
+            }
+        }
+
+        public void AddPlayer(Player p)
+        {
+            this.Members.Add(p);
+
+            _genderCounts[p.Gender]++;
+            this.GenderImbalance = Math.Abs(_genderCounts[Gender.Male] - _genderCounts[Gender.Female]);
+        }
+
+        public Player RemoveRandomPlayer()
+        {
+            int removeIndex = Globals.Rand.Next(0, this.Members.Count);
+            Player toRemove = this.Members[removeIndex];
+            this.Members.RemoveAt(removeIndex);
+
+            return toRemove;
         }
     }
 }
