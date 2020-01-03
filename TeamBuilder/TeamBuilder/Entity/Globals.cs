89dd67d3badaa86dac0741dbc9781e6d64fd6c9f
fistaco <fiske_schijlen@hotmail.com>
2020-01-03 16:17:38 +0100

    Add method to randomly shuffle a list.

--- TeamBuilder/TeamBuilder/Entity/Globals.cs
+++ TeamBuilder/TeamBuilder/Entity/Globals.cs
@@ -17,5 +17,28 @@ namespace TeamBuilder.Entity
             { "TableTennis", Sport.TableTennis },
             { "Volleyball", Sport.Volleyball }
         };
+
+        /// <summary>
+        /// Randomly shuffles a given list in-place.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static List<T> Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Swap the elem at index i with randomly chosen index j
+                int j = Rand.Next(i, n);
+                T elem1 = list[i];
+                T elem2 = list[j];
+
+                list[i] = elem2;
+                list[j] = elem1;
+            }
+
+            return list;
+        }
     }
 }
