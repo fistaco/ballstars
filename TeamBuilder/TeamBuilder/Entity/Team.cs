2b882e38392a7aea90000788782645ff57f1fa88
fistaco <fiske_schijlen@hotmail.com>
2020-01-03 14:02:45 +0100

    Update sport counts and amount of organisers after a team modification.

--- TeamBuilder/TeamBuilder/Entity/Team.cs
+++ TeamBuilder/TeamBuilder/Entity/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TeamBuilder.Entity
 {
@@ -7,16 +8,26 @@ namespace TeamBuilder.Entity
     {
         public List<Player> Members;
 
-        public int GenderImbalance = 0;
+        public int GenderImbalance;
+        public int SportImbalance;
+        public int AmountOfOrganisers;
+
         private readonly Dictionary<Gender, int> _genderCounts = new Dictionary<Gender, int>()
         {
             { Gender.Male, 0 },
             { Gender.Female, 0 }
         };
 
-        public int AmountOfOrganisers = 0;
-
-        public int SportImbalance = 0;
+        private readonly Dictionary<Sport, int> _sportCounts = new Dictionary<Sport, int>()
+        {
+            {Sport.Badminton, 0},
+            {Sport.Basketball, 0},
+            {Sport.Floorball, 0},
+            {Sport.Korfball, 0},
+            {Sport.Squash, 0},
+            {Sport.TableTennis, 0},
+            {Sport.Volleyball, 0}
+        };
 
         public Team(List<Player> members)
         {
@@ -29,9 +40,7 @@ namespace TeamBuilder.Entity
         public void AddPlayer(Player p)
         {
             this.Members.Add(p);
-
-            _genderCounts[p.Gender]++;
-            this.GenderImbalance = Math.Abs(_genderCounts[Gender.Male] - _genderCounts[Gender.Female]);
+            this.UpdateTeamProperties(p, true);
         }
 
         public Player RemoveRandomPlayer()
@@ -40,7 +49,25 @@ namespace TeamBuilder.Entity
             Player toRemove = this.Members[removeIndex];
             this.Members.RemoveAt(removeIndex);
 
+            this.UpdateTeamProperties(toRemove, false);
+
             return toRemove;
+        }
+
+        private void UpdateTeamProperties(Player p, bool added)
+        {
+            // Add or subtract 1 from the counts for a player addition/removal
+            int countModification = added ? 1 : -1;
+            _genderCounts[p.Gender] += countModification;
+            _sportCounts[p.Sport] += countModification;
+            if (p.InOrganisation)
+            {
+                this.AmountOfOrganisers += countModification;
+            }
+
+            // Update imbalances by using the gender and sport counts of which we're keeping track
+            this.GenderImbalance = Math.Abs(_genderCounts[Gender.Male] - _genderCounts[Gender.Female]);
+            this.SportImbalance = this._sportCounts.Count(pair => pair.Value != 1);
         }
     }
 }
