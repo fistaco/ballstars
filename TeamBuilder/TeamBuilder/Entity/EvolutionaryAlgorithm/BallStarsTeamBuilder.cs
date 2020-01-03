e770f9294cbc22f27b20a7c2b417a3c1142725c5
fistaco <fiske_schijlen@hotmail.com>
2020-01-03 17:24:16 +0100

    Write template for the EA's main loop.

--- TeamBuilder/TeamBuilder/Entity/EvolutionaryAlgorithm/BallStarsTeamBuilder.cs
+++ TeamBuilder/TeamBuilder/Entity/EvolutionaryAlgorithm/BallStarsTeamBuilder.cs
@@ -7,12 +7,46 @@ namespace TeamBuilder.Entity.EvolutionaryAlgorithm
 {
     class BallStarsTeamBuilder : EvolutionaryAlgorithm
     {
+        private BallStarsTeamSet _initialTeams;
+
+        private List<Player> _players = new List<Player>(); // TODO: initialise _players using given input file
+
+        private readonly int _teamSize;
+
+        public BallStarsTeamBuilder(string fileName, int teamSize)
+        {
+            _initialTeams = new BallStarsTeamSet(fileName);
+            _teamSize = teamSize;
+        }
+
         public override void Run()
         {
             // Construct n random solutions, which are permutations of one original random set
             List<Individual.Individual> population = InitRandomPopulation(65536);
 
             // Evolve over generations until a sufficiently good solution is found or time runs out.
+            float bestFitness = _initialTeams.Evaluate();
+            BallStarsTeamSet bestSolution = _initialTeams;
+            while (bestFitness != 0f) // TODO: Include timer
+            {
+                // Create offspring using recombination and mutation so we'll have 2n individuals
+                // Select best n individuals or do tournament select
+                
+                // Evaluate the selected individuals and update bestFitness
+                foreach (var individual in population)
+                {
+                    float fitness = individual.Evaluate();
+                    if (fitness < bestFitness)
+                    {
+                        bestFitness = fitness;
+                        bestSolution = individual as BallStarsTeamSet;
+
+                        Console.WriteLine($"New best fitness: {bestFitness}");
+
+                        // TODO: Save the solution in a file or pretty-print it
+                    }
+                }
+            }
 
             throw new NotImplementedException();
         }
