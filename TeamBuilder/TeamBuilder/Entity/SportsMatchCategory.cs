40093d97a0283db1e53b70dd233624fe8a361321
fistaco <fiske_schijlen@hotmail.com>
2020-02-25 17:39:35 +0100

    Create a specific class for a sports match category.

---
+++ TeamBuilder/TeamBuilder/Entity/SportsMatchCategory.cs
@@ -0,0 +1,20 @@
+namespace TeamBuilder.Entity
+{
+    public enum SportsMatchCategory
+    {
+        // Regular categories
+        Badminton,
+        Basketball,
+        Floorball,
+        Korfball,
+        Squash,
+        TableTennis,
+        Volleyball,
+        // Doubles
+        BadmintonDoubles,
+        TableTennisDoubles,
+        // Misc
+        Referee,
+        Break
+    }
+}
