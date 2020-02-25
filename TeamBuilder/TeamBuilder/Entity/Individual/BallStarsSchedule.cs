ff8e74521cd5e324662b3833231f9b67a86cc932
fistaco <fiske_schijlen@hotmail.com>
2020-02-25 17:44:30 +0100

    Define a MatchPool with which we can generate events.

--- TeamBuilder/TeamBuilder/Entity/Individual/BallStarsSchedule.cs
+++ TeamBuilder/TeamBuilder/Entity/Individual/BallStarsSchedule.cs
@@ -10,6 +10,31 @@ namespace TeamBuilder.Entity.Individual
         {
             this.Rounds = new RoundPlanning[timeSlotAmount];
         }
+
+        public List<SportsMatch> InitialiseMatchPool()
+        {
+            // Each match has a category and an amount of players to be allotted per team
+            return new List<SportsMatch>()
+            {
+                new SportsMatch(SportsMatchCategory.Badminton, 1),
+                new SportsMatch(SportsMatchCategory.Badminton, 2),
+                new SportsMatch(SportsMatchCategory.BadmintonDoubles, 2),
+                new SportsMatch(SportsMatchCategory.Basketball, 5),
+                new SportsMatch(SportsMatchCategory.Basketball, 6), // 1 reserve
+                new SportsMatch(SportsMatchCategory.Floorball, 4),
+                new SportsMatch(SportsMatchCategory.Floorball, 5),
+                new SportsMatch(SportsMatchCategory.Korfball, 8),
+                new SportsMatch(SportsMatchCategory.Squash, 1),
+                new SportsMatch(SportsMatchCategory.Squash, 2),
+                new SportsMatch(SportsMatchCategory.Squash, 3),
+                new SportsMatch(SportsMatchCategory.TableTennis, 1),
+                new SportsMatch(SportsMatchCategory.TableTennis, 2),
+                new SportsMatch(SportsMatchCategory.TableTennis, 3),
+                new SportsMatch(SportsMatchCategory.TableTennisDoubles, 2),
+                new SportsMatch(SportsMatchCategory.Volleyball, 6),
+                new SportsMatch(SportsMatchCategory.Referee, 1)
+            };
+        }
         
         public override float Evaluate()
         {
