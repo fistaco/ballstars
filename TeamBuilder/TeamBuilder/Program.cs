5e626c0c74a6a1a7984f4fbd1212d377c54c5138
fistaco <fiske_schijlen@hotmail.com>
2020-02-25 12:39:53 +0100

    Add test code for debugging to Main.

--- TeamBuilder/TeamBuilder/Program.cs
+++ TeamBuilder/TeamBuilder/Program.cs
@@ -8,8 +8,16 @@ namespace TeamBuilder
         static void Main(string[] args)
         {
             // Require either 2 or 3 arguments
-            if (args.Length > 3)
+            if (args.Length < 2 || args.Length > 3)
             {
+                // // TODO: remove this test stuff later
+                // string testcsvFilename = "./../../../tests/test-cases/test-input-64-player-converted-sport-names.csv";
+                // int testteamSize = 8;
+                // string testoutputFile = "test_ballstars_teams.csv";
+                //
+                // var testteamBuilder = new BallStarsTeamBuilder(testcsvFilename, testteamSize, testoutputFile);
+                // testteamBuilder.Run();
+
                 Console.WriteLine("Usage: teambuilder players_file avg_team_size [output_file]");
                 Environment.Exit(0);
             }
