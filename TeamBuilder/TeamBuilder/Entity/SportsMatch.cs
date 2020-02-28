2d19a1e996b4f2a44d68e6d860b8c6d8961b1675
fistaco <fiske_schijlen@hotmail.com>
2020-02-28 13:04:24 +0100

    Introduce possibility to create a random SportsEvent from a given pool.

--- TeamBuilder/TeamBuilder/Entity/SportsMatch.cs
+++ TeamBuilder/TeamBuilder/Entity/SportsMatch.cs
@@ -1,10 +1,35 @@
+using System.Collections.Generic;
+
 namespace TeamBuilder.Entity
 {
     public class SportsMatch
     {
-        public Sport Sport;
+        public SportsMatchCategory MatchType;
         public int PlayersPerTeam;
+
+        public SportsMatch(SportsMatchCategory matchType, int playersPerTeam)
+        {
+            this.MatchType = matchType;
+            this.PlayersPerTeam = playersPerTeam;
+        }
+
+        /// <summary>
+        /// Returns a copy of a random SportsMatch event from a given pool.
+        /// </summary>
+        /// <returns></returns>
+        public static SportsMatch Random(List<SportsMatch> matchPool)
+        {
+            int randIndex = Globals.Rand.Next(0, matchPool.Count);
+            return matchPool[randIndex].Clone();
+        }
         
-        public SportsMatch() { }
+        /// <summary>
+        /// Returns a copy of this SportsMatch object.
+        /// </summary>
+        /// <returns></returns>
+        public SportsMatch Clone()
+        {
+            return new SportsMatch(this.MatchType, this.PlayersPerTeam);
+        }
     }
 }
