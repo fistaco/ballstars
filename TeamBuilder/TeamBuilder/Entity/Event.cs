ce431f96307dc6637c59cca095212281d51045ec
fistaco <fiske_schijlen@hotmail.com>
2020-02-28 13:05:04 +0100

    Introduce possibility to create a random Event with set team IDs.

--- TeamBuilder/TeamBuilder/Entity/Event.cs
+++ TeamBuilder/TeamBuilder/Entity/Event.cs
@@ -9,12 +9,31 @@ namespace TeamBuilder.Entity
 
         public List<SportsMatch> Matches;
 
+        /// <summary>
+        /// Constructs an event with an empty list of matches for the two given team IDs.
+        /// </summary>
+        /// <param name="teamOneId"></param>
+        /// <param name="teamTwoId"></param>
         public Event(int teamOneId, int teamTwoId)
         {
             this.TeamOneId = teamOneId;
             this.TeamTwoId = teamTwoId;
             
             this.Matches = new List<SportsMatch>();
+        }
+
+        public static Event Random(int teamOneId, int teamTwoId, List<SportsMatch> matchPool)
+        {
+            var evnt = new Event(teamOneId, teamTwoId);
+            
+            // Add 1 to 3 random SportsMatch objects from the given pool.
+            int amountToAdd = Globals.Rand.Next(1, 4);
+            for (int i = 0; i < amountToAdd; i++)
+            {
+                evnt.Matches.Add(SportsMatch.Random(matchPool));
+            }
+
+            return evnt;
         }
     }
 }
