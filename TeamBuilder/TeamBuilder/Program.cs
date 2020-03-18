1c5a515243a238a9fb7c9031737d1fe6dd159550
fistaco <fiske_schijlen@hotmail.com>
2020-03-18 16:23:32 +0100

    Make the scheduler runnable from a terminal.

--- TeamBuilder/TeamBuilder/Program.cs
+++ TeamBuilder/TeamBuilder/Program.cs
@@ -7,19 +7,54 @@ namespace TeamBuilder
     {
         static void Main(string[] args)
         {
+            RunSchedulePlanner(args);
+            // RunTeamBuilder(args);
+        }
+
+        private static void RunTeamBuilder(string[] allArgs)
+        {
             // Require either 2 or 3 arguments
-            if (args.Length < 2 || args.Length > 3)
+            if (allArgs.Length < 2 || allArgs.Length > 3)
             {
                 Console.WriteLine("Usage: teambuilder players_file avg_team_size [output_file]");
                 Environment.Exit(0);
             }
 
-            string csvFilename = args[0];
-            int teamSize = int.Parse(args[1]);
-            string outputFile = args.Length == 3 ? args[2] : "ballstars_teams.csv";
+            string csvFilename = allArgs[0];
+            int teamSize = int.Parse(allArgs[1]);
+            string outputFile = allArgs.Length == 3 ? allArgs[2] : "ballstars_teams.csv";
 
             var teamBuilder = new BallStarsTeamBuilder(csvFilename, teamSize, outputFile);
             teamBuilder.Run();
+        }
+
+        private static void RunSchedulePlanner(string[] allArgs)
+        {
+            // Require at least 3 arguments
+            if (allArgs.Length < 3)
+            {
+                Console.WriteLine("Usage: scheduler #rounds #teams #players_per_team [output_file]");
+                Environment.Exit(0);   
+            }
+            
+            int amountOfRounds = int.Parse(allArgs[0]);
+            int amountOfTeams = int.Parse(allArgs[1]);
+            int avgPlayersPerTeam = int.Parse(allArgs[2]);
+            string outputFile = allArgs.Length > 3 ?
+                allArgs[3] :
+                $"schedule_{amountOfRounds}-rounds_{amountOfTeams}-teams_{avgPlayersPerTeam}-ppt.csv";
+            string[] teamNames = new string[amountOfTeams];
+
+            new BallStarsSchedulePlanner(
+                    amountOfRounds,
+                    amountOfTeams,
+                    avgPlayersPerTeam,
+                    outputFile,
+                    usePredefinedMatchUps: true,
+                    useLocalSearch: false,
+                    useCrossover: false,
+                    teamNames)
+                .Run();
         }
     }
 }
